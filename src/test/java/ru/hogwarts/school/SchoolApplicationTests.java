4dd693fafc9d3aefb34f3ff524ee3ed39d07277f
Max <thepro545@mail.ru>
2022-05-10 19:20:55 +0300

    delTest

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,13 +1,13 @@
-package ru.hogwarts.school;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SchoolApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package ru.hogwarts.school;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SchoolApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
