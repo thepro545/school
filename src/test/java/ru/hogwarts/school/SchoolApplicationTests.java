07fda47dfc9e2d9665c67f2fd1ae60af87429e85
Max <thepro545@mail.ru>
2022-05-14 14:09:50 +0300

    fix

--- src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
+++ src/test/java/ru/hogwarts/school/SchoolApplicationTests.java
@@ -1,14 +1,24 @@
 package ru.hogwarts.school;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import ru.hogwarts.school.controller.StudentController;
 import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.repositories.StudentRepository;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SchoolApplicationTests {
@@ -22,6 +32,15 @@ class SchoolApplicationTests {
 	@Autowired
 	private TestRestTemplate restTemplate;
 
+	@MockBean
+	private StudentRepository mockRepository;
+
+	@BeforeEach
+	public  void init() {
+		Student student = new Student("BobTest", 99);
+		when(mockRepository.findById(1L)).thenReturn(Optional.of(student));
+	}
+
 	@Test
 	public void testGetStudent() throws Exception {
 		Assertions
@@ -32,9 +51,7 @@ class SchoolApplicationTests {
 	@Test
 	public void testPostStudent() throws Exception {
 		Student student = new Student();
-		student.setId(7L);
-		student.setName("BobTest");
-		student.setAge(99);
+		when(mockRepository.save(any(Student.class))).thenReturn(student);
 
 		Assertions
 				.assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/student", student, String.class))
@@ -44,24 +61,32 @@ class SchoolApplicationTests {
 	@Test
 	public void testUpdateStudent() throws Exception {
 		Student newStudent = new Student();
-		newStudent.setId(7L);
+		newStudent.setId(7);
 		newStudent.setName("BobTest1");
 		newStudent.setAge(100);
+		HttpEntity<Student> entity = new HttpEntity<>(newStudent, new HttpHeaders());
+		when(mockRepository.save(any(Student.class))).thenReturn(newStudent);
+
 
 		Assertions
-				.assertThat(this.restTemplate.put("http://localhost:" + port + "/student", newStudent, String.class))
+				.assertThat(this.restTemplate.exchange("http://localhost:" + port + "/student", HttpMethod.PUT, entity, String.class))
 				.isNotNull();
 	}
 
 	@Test
 	public void testRemoveStudent() throws Exception {
 		Student newStudent = new Student();
-		newStudent.setId(7L);
+		newStudent.setId(7);
 		newStudent.setName("BobTest1");
 		newStudent.setAge(100);
+		HttpEntity<Student> entity = new HttpEntity<>(null, new HttpHeaders());
+		when(mockRepository.save(any(Student.class))).thenReturn(newStudent);
+
 
 		Assertions
-				.assertThat(this.restTemplate.delete("http://localhost:" + port + "/student" + newStudent.getId()))
-				.isNull();
+				.assertThat(this.restTemplate.exchange("http://localhost:" + port + "/student", HttpMethod.DELETE, entity, String.class))
+				.isNotNull();
 	}
+
+
 }
