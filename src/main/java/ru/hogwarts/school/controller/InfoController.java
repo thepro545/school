733ec24e587c67db15cb139c491fef08a6ed432d
Max <thepro545@mail.ru>
2022-06-11 10:39:10 +0300

    done

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,7 +1,6 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,11 +24,15 @@ public class InfoController {
     @GetMapping("/getSum")
     public Integer getSum() {
 
-       long start = System.currentTimeMillis();
-       int sum =  Stream.iterate(1, a -> a +1)
+        List<Integer> sum =  Stream.iterate(1, a -> a +1)
+                .limit(10_000_000)
+                .collect(Collectors.toList());
+
+        long start = System.currentTimeMillis();
+        int sum1 =  sum.stream()
                .parallel()
-               .limit(10_000_000)
-                .reduce(0, (a, b) -> a + b );
+               .mapToInt(i -> i)
+               .reduce(0, (a, b) -> a + b );
         return (int) (System.currentTimeMillis() - start);
     }
 
